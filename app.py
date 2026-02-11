a3f7840ac265dadc84b9a7cc8b592c8eee0c4b1c
sanjaywarriorsway <warriorswayhub@gmail.com>
2026-02-11 01:21:25 +0000

    Day 4: Model Update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 # 1. Load the Model
-with open("model.pkl", "rb") as f:
+with open("model2.pkl", "rb") as f:
     model = pickle.load(f)
 
 app = FastAPI()
--- model_train.py
+++ model_train.py
@@ -1,17 +1,19 @@
 import pickle
 from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
+from sklearn import tree
 
 # 1. Load Data (Iris Flower Dataset)
 X, y = load_iris(return_X_y=True)
 
 # 2. Train Model
 print("Training model...")
-clf = LogisticRegression(max_iter=200)
+#clf = LogisticRegression(max_iter=200)
+clf = tree.DecisionTreeClassifier()
 clf.fit(X, y)
 
 # 3. Save Model (The "Artifact")
-with open("model.pkl", "wb") as f:
+with open("model2.pkl", "wb") as f:
     pickle.dump(clf, f)
 
 print("Success! Model trained and saved to model.pkl")
